2245334f381b1b94fa0ab68f546ee40ecb90e3f6
Carlos Marques <psycarlo1@gmail.com>
2025-01-29 20:58:56 +0000

    feat: faqs

--- components/RSFAQModal.vue
+++ components/RSFAQModal.vue
@@ -20,23 +20,76 @@
         <DialogDescription class="text-center text-brand-primary">
           Your <span class="font-medium">questions</span> answered here.
         </DialogDescription>
-        <div class="mt-4 flex flex-col gap-4">
+        <div class="mt-4 flex flex-col gap-4 overflow-y-auto">
           <div class="flex flex-col gap-1">
-            <span class="text-lg font-medium text-brand-primary"
-              >Where is the event?</span
-            >
+            <span class="text-lg font-medium text-brand-primary">
+              What is the Remigration Summit?
+            </span>
             <p class="text-brand-primary">
-              Italy, Milan. The exact place will remain secret until right
-              before the conference. Exact place will be sent to ticket holders
-              2 or 3 days before the conference.
+              The Remigration Summit 2025 is a yearly event about Remigration
+              and its political, cultural and social pursuit. It also aims at
+              being an identarian reunion gathering together activists,
+              influencers, political parties and movements and independent
+              media.
             </p>
           </div>
           <div class="flex flex-col gap-1">
-            <span class="text-lg font-medium text-brand-primary"
-              >When is the event?</span
-            >
+            <span class="text-lg font-medium text-brand-primary">
+              Do I need to register in advance?
+            </span>
             <p class="text-brand-primary">
-              May 17th, in the afternoon. Exact time given with exact place.
+              Yes, we recommend registering in advance, as places are limited.
+            </p>
+          </div>
+          <div class="flex flex-col gap-1">
+            <span class="text-lg font-medium text-brand-primary">
+              Is there parking available on site?
+            </span>
+            <p class="text-brand-primary">
+              Yes, parking is available for participants. We also encourage the
+              use of public transport; all transport and location details will
+              be sent to ticket buyers 48 to 72 hours before the event.
+            </p>
+          </div>
+          <div class="flex flex-col gap-1">
+            <span class="text-lg font-medium text-brand-primary">
+              Will there be simultaneous translation into other languages?
+            </span>
+            <p class="text-brand-primary">
+              No, the event will be entirely in English.
+            </p>
+          </div>
+          <div class="flex flex-col gap-1">
+            <span class="text-lg font-medium text-brand-primary">
+              Is there merchandising or material available?
+            </span>
+            <p class="text-brand-primary">
+              Yes, we will have a merchandising area with products related to
+              the event theme.
+            </p>
+          </div>
+          <div class="flex flex-col gap-1">
+            <span class="text-lg font-medium text-brand-primary">
+              Will there be accommodation close to the site?
+            </span>
+            <p class="text-brand-primary">Yes, hotels will be close by.</p>
+          </div>
+          <div class="flex flex-col gap-1">
+            <span class="text-lg font-medium text-brand-primary">
+              Is the event safe?
+            </span>
+            <p class="text-brand-primary">
+              Yes, the event will have private security to ensure the comfort
+              and well-being of all participants.
+            </p>
+          </div>
+          <div class="flex flex-col gap-1">
+            <span class="text-lg font-medium text-brand-primary">
+              How can I stay up to date with the event?
+            </span>
+            <p class="text-brand-primary">
+              Follow us on social media to receive all the updates. Links on the
+              page.
             </p>
           </div>
         </div>
